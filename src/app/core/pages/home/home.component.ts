1eb8ad4f754a2f16c1f1706028386b368f496027
Kleber Vasconcelos <klebervasconcelos@Macbook-Pro.local>
2024-02-12 16:31:38 -0300

    Layout added

--- src/app/core/pages/home/home.component.ts
+++ src/app/core/pages/home/home.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../../shared/components/header/header.component';
+import { CardComponent } from '../../../shared/components/card/card.component';
+import { FooterComponent } from '../../../shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
+  imports: [HeaderComponent, CardComponent, FooterComponent],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  isOpen = false;
+
+  open() {
+    this.isOpen = !this.isOpen;
+  }
+}
